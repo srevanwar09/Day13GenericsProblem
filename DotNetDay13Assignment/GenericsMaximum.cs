55e90c04bbb8516fd8ea7c386c74509209c8174c
srevanwar09 <srevanwar09@domain.com>
2022-09-10 16:46:25 +0530

    Gnerics find max program

---
+++ DotNetDay13Assignment/GenericsMaximum.cs
@@ -0,0 +1,33 @@
+﻿
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsFindMaximum
+    {
+        public class GenericsMaximum<T> where T : IComparable
+        {
+            public T[] value;
+            public GenericsMaximum(T[] value)//this constructor is used to initialize the value 
+            {
+                this.value = value;
+            }
+            public T[] Sort(T[] values)//this method is used to sort the values array
+            {
+                Array.Sort(values);
+                return values;
+            }
+            public T MaxValue(params T[] values) // this method used to find the maximum value
+            {
+                var sorted_values = Sort(values);
+                return sorted_values[sorted_values.Length - 1];
+            }
+            public void PrintMaxValue()// this method is used to print the maximum value received from MaxValue method
+            {
+                var max = MaxValue(this.value);
+                Console.WriteLine("Maximum value is " + max);
+            }
+        }
+    }
